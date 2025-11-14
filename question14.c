704c82971d57f06d3d5f2f1605f797dd5f587427
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-11-14 15:54:05 -0600

    updated question14.c to return 0 in main()

--- question14.c
+++ question14.c
@@ -6,5 +6,5 @@ int main(){
 	printf("Enter an integer to determine positive, negative, or zero: \n");
 	scanf("%d",&x);
 	(x == 0) ? printf("number is zero\n") : ((x > 0) ? printf("number is positive\n") : printf("number is negative\n"));
-
+	return 0;
 }
