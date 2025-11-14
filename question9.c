9e5afd5a56e3078f0f99fb9fcd568657cc7908b3
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-11-14 15:13:26 -0600

    finished question9.c

---
+++ question9.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+
+	int x, y, d;
+	float i,e;
+
+	printf("Input two integers to show both integer and floating-point division results (x / y). \n");
+	printf("Enter the first integer, x: \n");
+	scanf("%d", &x);
+
+	printf("Enter the second integer, y: \n");
+	scanf("%d",&y);
+
+	d = x / y;
+	i = x / y;
+	e = (float) x / y;
+	
+	printf("Integer division result: %d\n",d);
+	printf("Implicit casting floating-point division result: %.2f\n",i);
+	printf("Explicit casting floating-point division result: %.2f\n",e);
+
+}
