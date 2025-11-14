1be568ede5488dc179304483468e8b8a94f99b0f
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-11-14 15:19:13 -0600

    finished question10.c

---
+++ question10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+	char c;
+	int i;
+	long int li;
+	float f;
+	double d;
+
+	printf("Size of char c: %zu\n", sizeof(c));
+        printf("Size of int i: %zu\n", sizeof(i));
+        printf("Size of long int li: %zu\n", sizeof(li));
+        printf("Size of float f: %zu\n", sizeof(f));
+        printf("Size of double d: %zu\n", sizeof(d));
+
+
+}
