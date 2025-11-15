73d61781bff93a4ab6176388e4f5e5a998c70c12
Sebastian-Tran <Sebastian.Tran@tcu.edu>
2025-11-14 20:37:18 -0600

    updated question4.c

--- question4.c
+++ question4.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-    //Original Line declared a and b as integers, but that results in a typecasting error when being used to calculate float avg.
-    float a = 5, b = 10;
-    float avg = (a + b) / 2;
+    int a = 5, b = 10;
+    float avg = (float) (a + b) / 2; //integers were originally implicitly typecasted, but that resulted in wrong value.
     printf("Average = %.2f\n", avg);
     return 0;
 }
